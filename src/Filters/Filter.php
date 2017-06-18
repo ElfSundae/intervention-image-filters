90a2063737e3f881b3183a3a6e3bce796b40cfa2
Elf Sundae <elf.sundae@gmail.com>
2017-06-19 07:16:54 +0800

    Add Filters

---
+++ src/Filters/Filter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ElfSundae\InterventionImage\Filters;
+
+use Exception;
+use Intervention\Image\Filters\FilterInterface;
+
+abstract class Filter implements FilterInterface
+{
+    /**
+     * Handle dynamic method calls to set properties.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function __call($method, $parameters)
+    {
+        if (property_exists($this, $method) && ! empty($parameters)) {
+            $this->{$method} = $parameters[0];
+
+            return $this;
+        }
+
+        throw new Exception('Call to undefined method '.get_class($this)."::{$method}()");
+    }
+}
